ee229c0582e53d51755149bbbb6146d25f2a8eee
Gabriel <GabrielTeixeira28>
2021-02-23 12:30:36 -0300

    Ultimas alteracoes - day 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,27 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app =  express();
 
-/* 
-GET => BUSCA
-POST => SALVAR
-PUT => ALTERAR
-DELETE => DELETAR
-PATCH => ALTERAÇÃO ESPECIFICA
-*/
-
-// http://localhost:3333/users
-
-app.get("/users", (requeste, response) => {
-return response.json({ message: "Hello World"});
-});
-
-//1 param => Rota(Recurso API)
-//2 param => request,response
-
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
 
